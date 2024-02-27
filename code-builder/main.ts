dd6b8d5dc0508aba977a74e5869f7b281d5fc8dc
pavanleton <pavankumarkpkp@gmail.com>
2024-02-27 17:48:20 +0530

    setup aws client sdk to upload to s3

--- code-builder/main.ts
+++ code-builder/main.ts
@@ -1,8 +1,21 @@
 import {exec} from "child_process"
 import path from "path"
-import fs from "fs/promises"
+import fsp from "fs/promises"
+import fs from "fs"
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import mime from "mime-types"
+
+const s3 =  new S3Client({
+    region: "ap-south-1",
+    credentials: {
+        accessKeyId: "",
+        secretAccessKey: ""
+    }
+})
+const PROJECT_ID = process.env.PROJECT_ID
 
 function main() {
+    if(!PROJECT_ID) throw Error("PROJECT_ID not found")
     const outputDir = path.join(__dirname, 'source-code')
 
     const p = exec(`cd ${outputDir} && npm i && npm run build`)
@@ -19,11 +32,11 @@ function main() {
         console.log('Build Complete, uploading to S3')
 
         const distDirPath = path.join(outputDir, 'dist')
-        const distDirContents = await fs.readdir(distDirPath)
+        const distDirContents = await fsp.readdir(distDirPath)
 
         for(const file of distDirContents) {
             const filePath = path.join(distDirPath, file)
-            const stats = await fs.lstat(filePath)
+            const stats = await fsp.lstat(filePath)
 
             if(stats.isDirectory()) continue;
 
@@ -31,7 +44,14 @@ function main() {
 
             // TODO: upload to s3
             console.log(`Uploading ${filePath}`)
+            const command = new PutObjectCommand({
+                Bucket: "percel",
+                Key: `${PROJECT_ID}/${file}`,
+                Body: fs.createReadStream(filePath),
+                ContentType: mime.lookup(filePath) as string
+            });
 
+            await s3.send(command)
             console.log('uploaded')
         }
 

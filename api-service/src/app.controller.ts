f749c6897615ab4d0d14b717f1138c8be602fd3f
pavanleton <pavankumarkpkp@gmail.com>
2024-03-07 19:51:04 +0530

    rm: loggers

--- api-service/src/app.controller.ts
+++ api-service/src/app.controller.ts
@@ -13,28 +13,11 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    this.logger.log({
-      level: 'info',
-      message: `User created successfully - user.id`,
-    });
     return this.appService.getHello();
   }
 
   @Post('/create')
   async createDeployment(@Body() payload: CreateDeploymentDto) {
-    this.logger.log({
-      level: '1',
-      message: `User created successfully - user.id`,
-    });
-    console.log({
-      level: '1',
-      message: "Yo, I'm coming from the server from console lol",
-    });
     return this.appService.createDeployment(payload);
   }
 }
-
-// p: rDOF*4U-FKyjtoNfzzQC
-// u: elastic
-// HTTP CA certificate SHA-256 fingerprint:
-//   bb82ea65e39ac87d5b2aded4a8bcea233c64fa5fefdf81a8f9546f1ae456dae1
